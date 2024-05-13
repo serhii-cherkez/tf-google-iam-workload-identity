be247ba6558c349f1d78c3e0ce41c02f105d4d40
Serhii Chekez <serhii.cherkez@gmail.com>
2024-05-13 17:34:18 +0000

    fix

--- main.tf
+++ main.tf
@@ -57,8 +57,8 @@ resource "google_project_iam_member" "this" {
   for_each = toset([
      "roles/editor",
      "roles/secretmanager.secretAccessor", 
-     "roles/iam.serviceAccountTokenCreator",
-     "roles/cloudkms.cryptoKeyVersions.useToEncrypt"])
+     "roles/iam.serviceAccountTokenCreator"
+     ])
   role    = each.key
   member  = "serviceAccount:${google_service_account.this.email}"
 }
